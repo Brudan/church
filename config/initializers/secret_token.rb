0e6de4459f1850011863d82de1a9b64dfd57d196
danogwok <danogwok@yahoo.co.uk>
2013-04-09 01:47:23 +0300

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Church::Application.config.secret_token = 'efc9f203dbea5b70200ce4bd6ebcfdad4076b4d8c424fd56d99ecf7674c6b7969e71f4875d00e13a67937b3612a59ddaa8442e01bbbb52f83d8a2214ff2f9b71'
